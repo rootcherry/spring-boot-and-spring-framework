5a5361812a8e312b5a437d013ebd931b4c105c5e
Chris N <hi.chrisn@gmail.com>
2024-09-30 22:34:35 -0300

    java spring bean

--- 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,8 +1,14 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Configure the things that we want Spring to manage
 @Configuration
 public class HelloWorldConfiguration {
 	
+	@Bean
+	public String name() {
+		return "John";
+	}
 }
