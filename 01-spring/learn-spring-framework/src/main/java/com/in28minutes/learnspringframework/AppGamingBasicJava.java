eef01e48e3abc3b4412643f2e274c2239bae1617
Chris N <hi.chrisn@gmail.com>
2024-09-30 22:07:32 -0300

    adding pacman game

--- 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -3,13 +3,15 @@ package com.in28minutes.learnspringframework;
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		
-		var game = new MarioGame();
+//		var game = new MarioGame();
 //		var game = new SuperContraGame();
+		var game = new PacmanGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 
