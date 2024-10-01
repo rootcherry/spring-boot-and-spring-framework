7432b190d589ea1de42aa8798c49e7e0ce7e553f
Chris N <hi.chrisn@gmail.com>
2024-09-30 21:55:10 -0300

    java interface to make app loosely coupled

--- 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,9 +1,9 @@
 package com.in28minutes.learnspringframework.game;
 
 public class GameRunner {
-	private SuperContraGame game;
+	private GamingConsole game;
 	
-	public GameRunner(SuperContraGame game) {
+	public GameRunner(GamingConsole game) {
 		this.game = game;
 	}
 
