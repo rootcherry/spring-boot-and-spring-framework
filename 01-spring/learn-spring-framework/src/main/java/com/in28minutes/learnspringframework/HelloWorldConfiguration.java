90fef18a52937e4e29cb6042b46ccee944460052
Chris N <hi.chrisn@gmail.com>
2024-10-01 21:00:34 -0300

    java spring beans in spring java config file

--- 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ 01-spring/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,10 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {};
+
+record Address (String firstLine, String city) {};
+
 // Configure the things that we want Spring to manage
 @Configuration
 public class HelloWorldConfiguration {
@@ -10,5 +14,20 @@ public class HelloWorldConfiguration {
 	@Bean
 	public String name() {
 		return "John";
+	}
+	
+	@Bean
+	public int age() {
+		return 15;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Jane", 20);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Elm Street", "Springwood");
 	}
 }
