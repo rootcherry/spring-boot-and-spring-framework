9002376f0be82f52c2eac15ef837b2c16d74cf07
Chris N <hi.chrisn@gmail.com>
2024-10-03 11:12:51 -0300

    getting spring framework to create beans using @Component

--- 01-spring/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ 01-spring/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -2,23 +2,19 @@ package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.PacmanGame;
 
 @Configuration
+@ComponentScan("package com.in28minutes.learnspringframework.game")
 public class App03GamingSpringBeans {
 	
 	@Bean
-	public GamingConsole game() {
-		var game = new PacmanGame();
-		return game;
-	}
-	
-	@Bean
 	public GameRunner gameRunner(GamingConsole game) {
+		System.out.println("Parameter: "+ game);
 		var gameRunner = new GameRunner(game);
 		return gameRunner;
 	}
